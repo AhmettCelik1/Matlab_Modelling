a7cdf3e01ca4dd8d22ba27e5095b686de111c1c5
aeskuav <101452906+1000HPFERRARI@users.noreply.github.com>
2023-02-06 13:17:39 +0300

    Update forces_moments.m

--- m/forces_moments.m
+++ m/forces_moments.m
@@ -10,7 +10,7 @@
 %       wind  - wind vector in the inertial frame
 %
 
-function out = forces_moments(x, delta, wind, P, MAV)
+function out = forces_moments(x, delta, wind, P)
 
     % relabel the inputs
     pn      = x(1);
@@ -37,6 +37,24 @@ function out = forces_moments(x, delta, wind, P, MAV)
     w_wg    = wind(6); % gust along body z-axis
     
     % compute wind data in NED
+    %w_n = 0;
+    %w_e = 0;
+    %w_d = 0;
+    
+    % compute air data
+    %Va = 0;
+    %alpha = 0;
+    %beta = 0;
+    
+    % compute external forces and torques on aircraft
+    %Force(1) =  0;
+    %Force(2) =  0;
+    %Force(3) =  0;
+    
+    %Torque(1) = 0;
+    %Torque(2) = 0;   
+    %Torque(3) = 0;
+  % compute wind data in NED
     w_n = w_ns - u_wg;
     w_e = w_es - v_wg;
     w_d = w_ds - w_wg;
@@ -46,29 +64,33 @@ function out = forces_moments(x, delta, wind, P, MAV)
     alpha = atan((w-w_d)/(u-w_n));
     beta = asin((v-w_e)/Va);
     
-    C_x_alpha = -0.030*cos(alpha) + MAV.C_L_alpha*sin(alpha);
-    C_x_q_alpha = -MAV.C_D_q*cos(alpha) + MAV.C_L_q*sin(alpha);
-    C_x_delta_e_alpha = -MAV.C_D_delta_e*cos(alpha) + MAV.C_L_delta_e*sin(alpha);
-    C_z_alpha = -0.030*sin(alpha) - MAV.C_L_alpha*cos(alpha);
-    C_z_q_alpha = -MAV.C_D_q*sin(alpha) - MAV.C_L_q*cos(alpha);  
-    C_z_delta_e_alpha = -MAV.C_D_delta_e*sin(alpha) -  MAV.C_L_delta_e*cos(alpha);
+    C_x_alpha = -0.030*cos(alpha) + 5.61*sin(alpha);
+    C_x_q_alpha = -0.0*cos(alpha) + 7.95*sin(alpha);
+    C_x_delta_e_alpha = -0.0135*cos(alpha) + 0.13*sin(alpha);
+    C_z_alpha = -0.030*sin(alpha) - 5.61*cos(alpha);
+    C_z_q_alpha = -0.0*sin(alpha) - 7.95*cos(alpha);  
+    C_z_delta_e_alpha = -0.0135*sin(alpha) -  0.13*cos(alpha);
     n = 10;
 
-    J = Va / (n * MAV.D_prop);
-    C_T_J = MAV.C_T2 * J * J + MAV.C_T1 * J + MAV.C_T0;
-    C_T_Q = MAV.C_Q2 * J * J + MAV.C_Q1 * J + MAV.C_Q0;
-    T_p = MAV.rho * n * n * MAV.D_prop^4 * C_T_J; 
-    Q_p = MAV.rho * n * n * MAV.D_prop^5 * C_Q_J;
+    J = Va / (n * 0.508);
+    C_T_J = -0.1079 * J * J + -0.06044 * J + 0.09357;
+    C_Q_J = -0.01664 * J * J + 0.004970 * J + 0.005230;
+    T_p = 1.2682 * n * n * 0.508^4 * C_T_J; 
+    Q_p = 1.2682 * n * n * 0.508^5 * C_Q_J;
     % compute external forces and torques on aircraft
-    Force(1) =  -m*g*sin(theta) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(C_x_alpha + C_x_q_alpha * MAV.c * q/(2*Va)) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(C_x_delta_e_alpha*delta_e) + (T_p*delta_t*Va);%tp sıkıntı
-    Force(2) =  m*g*cos(theta)*sin(psi) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.C_Y_0 + MAV.C_Y_beta*beta + MAV.C_Y_p*MAV.b*p/(2*Va) + MAV.C_Y_r*MAV.b*r/(2*Va)) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.C_Y_delta_a*delta_a + MAV.C_Y_delta_r*delta_r); 
-    Force(3) =  m*g*cos(theta)*cos(psi) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(C_z_alpha + C_z_q_alpha * MAV.c * q/(2*Va) ) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(C_z_delta_e_alpha*delta_e);
+    Force(1) =  -11.0*9.81*sin(theta) + (1/2)*1.2682*Va*Va*0.55*(C_x_alpha + C_x_q_alpha * 0.19 * q/(2*Va)) + (1/2)*1.2682*Va*Va*0.55*(C_x_delta_e_alpha*delta_e) + (T_p*delta_t*Va);%tp sıkıntı
+    Force(2) = 11.0*9.81*cos(theta)*sin(psi) + (1/2)*1.2682*Va*Va*0.55*(0.0 + -0.98*beta + 0.0*2.90*p/(2*Va) + 0.0*2.90*r/(2*Va)) + (1/2)*1.2682*Va*Va*0.55*(0.075*delta_a + 0.19*delta_r); 
+    Force(3) = 11.0*9.81*cos(theta)*cos(psi) + (1/2)*1.2682*Va*Va*0.55*(C_z_alpha + C_z_q_alpha * 0.19 * q/(2*Va) ) + (1/2)*1.2682*Va*Va*0.55*(C_z_delta_e_alpha*delta_e);
     
-    Torque(1) = (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.b*(MAV.C_L_0 + MAV.C_ell_beta*beta + MAV.C_ell_p*MAV.b*p/(2*Va) + MAV.C_ell_r*MAV.b*r/(2*Va))) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(b*(MAV.C_ell_delta_a*delta_a + MAV.C_ell_delta_r*delta_r) +  + Q_p );%Qp eksik
-    Torque(2) = (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.c*(MAV.C_m_0 + MAV.C_m_alpha*alpha + MAV.C_m_q*MAV.c*q/(2*Va))) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.c*(MAV.C_m_delta_e*delta_e));   
-    Torque(3) = (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.b*(MAV.C_n_0 + MAV.C_n_beta*beta + MAV.C_n_p*MAV.b*p/(2*Va) + MAV.C_n_r*MAV.b*r/(2*Va) )) + (1/2)*MAV.rho*Va*Va*MAV.S_wing*(MAV.b*(MAV.C_n_delta_a*delta_a + MAV.C_n_delta_r*delta_r));
+    Torque(1) = (1/2)*1.2682*Va*Va*0.55*(2.90*(0.23 + -0.13*beta + -0.51*2.90*p/(2*Va) + 0.25*2.90*r/(2*Va))) + (1/2)*1.2682*Va*Va*0.55*(2.90*(0.17*delta_a + 0.0024*delta_r) +  + Q_p );%Qp eksik
+    Torque(2) = (1/2)*1.2682*Va*Va*0.55*(0.19*(0.0135 + -2.74*alpha + -38.21*0.19*q/(2*Va))) + (1/2)*1.2682*Va*Va*0.55*(0.19*(-0.99*delta_e));   
+    Torque(3) = (1/2)*1.2682*Va*Va*0.55*(2.90*(0.0 + 0.073*beta + -0.069*2.90*p/(2*Va) + -0.095*2.90*r/(2*Va) )) + (1/2)*1.2682*Va*Va*0.55*(2.90*(-0.011*delta_a + -0.069*delta_r));
+   
+
    
     out = [Force'; Torque'; Va; alpha; beta; w_n; w_e; w_d];
+    fprintf("VAA");
+    disp(Va);
 end
 
 
